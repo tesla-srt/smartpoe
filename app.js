d9b18eac3635afbbf7b0a2bae6d7dea769b94d46
tesla-srt <71569052+tesla-srt@users.noreply.github.com>
2020-12-30 18:36:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ io.on('connection', socket => {
     //var contents = fs.readFileSync("./bin/all.json")
     var jsonContent = null;
 
-    exec("./bin/aaeonSmartPOE.exe all", (error, stdout, stderr) => {
+    /*exec("./bin/aaeonSmartPOE.exe all", (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`)
             return;
@@ -51,7 +51,7 @@ io.on('connection', socket => {
         jsonContent = JSON.parse(`${stdout}`)
         console.log(`test: ` + jsonContent.temp)
         console.log(`updated`)
-    });
+    });*/
 
     socket.on('update', data => {
         exec("./bin/aaeonSmartPOE.exe all", (error, stdout, stderr) => {
@@ -63,8 +63,8 @@ io.on('connection', socket => {
                 console.log(`stderr: ${stderr}`)
                 return;
             }
-            //contents = fs.readFileSync("./bin/all.json");
-            jsonContent = JSON.parse(JSON.stringify(stdout))
+            console.log(`${stdout}`)
+            jsonContent = JSON.parse(`${stdout}`)
             console.log(`test: ` + jsonContent.temp)
             console.log(`updated`)
         });
