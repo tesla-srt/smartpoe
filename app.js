45bebbbcd5b8c77be6214335b8081551c4b876c8
tesla-srt <71569052+tesla-srt@users.noreply.github.com>
2021-02-19 15:16:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -507,7 +507,7 @@ io.on('connection', socket => {
             let port3 = sp.ports[2];
             let port4 = sp.ports[3];
             console.log(jsonContent.length)
-            if (jsonContent.temp != 'N/A' && jsonContent.length > 0) {
+            if (jsonContent.temp != 'N/A' && jsonContent.length != null) {
                 try {
                     sp.temp = jsonContent.temp;
                     sp.location = config.info.location;
