13a5691681dafed3b404b47992cbddfc50e2670d
tesla-srt <71569052+tesla-srt@users.noreply.github.com>
2021-02-23 17:29:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ streamApp.get('/cam/:num/u/:user/p/:pass', async (req, res) => {
             curl.close();
         })
         .on('error', function (e) {
-            res.status(404);
+            //res.status(404);
             //let buffer = Buffer.from(fs.readFileSync('public/img/img404.png', 'utf-8')).toString('base64')
             //result = buffer
             res.sendFile(__dirname + '/public/img/img404.png')
